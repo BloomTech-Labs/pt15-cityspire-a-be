54b411c02cdcb11fb92634009733ee6b3ee254e6
Adam-Young <awy6448@gmail.com>
2021-02-04 21:35:37 -0500

    Created a table to save cities paired with userid

---
+++ data/migrations/20210204211825_saved_cities.js
@@ -0,0 +1,17 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('saved_cities', (table) => {
+    table.increments('id');
+    table.string('name').notNullable();
+    table.string('city_id').notNullable();
+    table.string('user_id')
+      .unsigned()
+      .notNullable()
+      .references('profiles.id')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('saved_cities');
+};
