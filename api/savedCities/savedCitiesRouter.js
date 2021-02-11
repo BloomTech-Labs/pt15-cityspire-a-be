cdfb65427822a37e4286f77a5e2216b110e41814
Adam Young <13275708+Awy64@users.noreply.github.com>
2021-02-10 22:13:05 -0500

    Saved cities endpoints (#2)
    * added info to readme.md
    
    * Created a table to save cities paired with userid
    
    * Created savedCitiesModel file for db functions
    
    * Created routes to save user sities by user id
    
    * Created middleware to check if a user owns fav.
    
    * Added routes to app.js
    
    * FIxed a bug where a function was misspelled and used prarams to get a id to delete.
    
    * Fixed  object being used for searching with findBy

---
+++ api/savedCities/savedCitiesRouter.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const authRequired = require('../middleware/authRequired');
+const ownsFavorite = require('../middleware/ownsFavorite');
+const router = express.Router();
+const savedCities = require('./savedCitiesModel');
+
+router.get('/', authRequired, function (req, res) {
+  savedCities
+    .findByUser_id(req.profile.id)
+    .then((cities) => {
+      res.status(200).json(cities);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: err.message });
+    });
+});
+
+router.post('/', authRequired, function (req, res) {
+  savedCities
+    .create(req.body)
+    .then((response) => {
+      res.status(201).json({ added: response });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: res.message });
+    });
+});
+
+router.delete('/:id', authRequired, ownsFavorite, function (req, res) {
+  savedCities
+    .remove(req.params.id)
+    .then(() => {
+      res.status(200).json({ message: 'deleted' });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message });
+    });
+});
+
+module.exports = router;
